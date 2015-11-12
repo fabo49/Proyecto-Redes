8a0096e6f22226f07fb7f4bcd65f7970411c0186
Fabian Rodriguez <farodriguez.49@gmail.com>
2015-11-11 23:55:50 -0600

    Se completa la validación de campos

--- SocketsAndroid/app/src/main/java/com/redes/fabian_stefano/sockets/Principal.java
+++ SocketsAndroid/app/src/main/java/com/redes/fabian_stefano/sockets/Principal.java
@@ -6,6 +6,7 @@ package com.redes.fabian_stefano.sockets;
  * @author Stefano Del Vecchio
  */
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,6 +16,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Principal extends AppCompatActivity {
 
@@ -23,6 +25,7 @@ public class Principal extends AppCompatActivity {
     private EditText input_mensaje;
     private EditText input_cant_veces;
     private FloatingActionButton fab;
+
     private ControladoraSocket m_controladora;
 
     @Override
@@ -31,13 +34,19 @@ public class Principal extends AppCompatActivity {
         setContentView(R.layout.activity_principal);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        edit_resultados = (EditText) findViewById(R.id.edit_resultado);
+        input_direccion = (EditText) findViewById(R.id.input_direccion);
+        input_mensaje = (EditText) findViewById(R.id.input_mensaje);
+        input_cant_veces = (EditText) findViewById(R.id.input_cant_veces);
 
         fab = (FloatingActionButton) findViewById(R.id.fab);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(input_direccion.getText().toString() != "") {
-                    m_controladora = new ControladoraSocket(input_direccion.getText().toString());
+                if(validaciones(v)){
+                    // TODO: 11/11/15
+                    //m_controladora = new ControladoraSocket(input_direccion.getText().toString());
                 }
             }
         });
@@ -51,10 +60,7 @@ public class Principal extends AppCompatActivity {
             }
         }
         * */
-        edit_resultados = (EditText) findViewById(R.id.edit_resultado);
-        input_direccion = (EditText) findViewById(R.id.input_direccion);
-        input_mensaje = (EditText) findViewById(R.id.input_mensaje);
-        input_cant_veces = (EditText) findViewById(R.id.input_cant_veces);
+
 
     }
 
@@ -85,5 +91,52 @@ public class Principal extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Método que realiza las validaciones de los datos en la interfaz.
+     * @author  Fabian Rodriguez
+     * @author Stefano del Vecchio
+     * @param v que es el View actual
+     * @return true si son validos los campos, false si no
+     */
+    private boolean validaciones(View v){
+        if(input_direccion.getText().length() != 0) {
+            if(input_mensaje.getText().length() != 0) {
+                if(input_cant_veces.getText().length() != 0){
+                    return true;
+                }else{
+                    Snackbar alerta = Snackbar.make(v, "Tiene que ingresar la cantidad de envíos", Snackbar.LENGTH_LONG);
+                    alerta.setAction("Revisar", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            input_cant_veces.requestFocus();
+                        }
+                    });
+                    alerta.show();
+                    return false;
+                }
+            }else{
+                Snackbar alerta = Snackbar.make(v, "Tiene que ingresar un mensaje", Snackbar.LENGTH_LONG);
+                alerta.setAction("Revisar", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        input_mensaje.requestFocus();
+                    }
+                });
+                alerta.show();
+                return false;
+            }
+        }else{
+            Snackbar alerta = Snackbar.make(v, "Tiene que ingresar una dirección IP", Snackbar.LENGTH_LONG);
+            alerta.setAction("Revisar", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    input_direccion.requestFocus();
+                }
+            });
+            alerta.show();
+            return false;
+        }
     }
 }
