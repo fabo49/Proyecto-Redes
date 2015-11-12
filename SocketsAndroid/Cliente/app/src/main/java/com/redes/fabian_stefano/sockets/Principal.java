e9bf3b7cad2047f9dcc2936bf63c723fc6b6cdf0
Fabian Rodriguez <farodriguez.49@gmail.com>
2015-11-12 15:50:46 -0600

    Se finaliza el cliente
    TO DO:
    * Servidor
    * Mediciones de tiempo

--- SocketsAndroid/Cliente/app/src/main/java/com/redes/fabian_stefano/sockets/Principal.java
+++ SocketsAndroid/Cliente/app/src/main/java/com/redes/fabian_stefano/sockets/Principal.java
@@ -7,6 +7,7 @@ package com.redes.fabian_stefano.sockets;
  */
 
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -18,15 +19,20 @@ import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Principal extends AppCompatActivity {
 
-    private EditText edit_resultados;
+    private TextView text_resultados;
     private EditText input_direccion;
-    private EditText input_mensaje;
     private EditText input_cant_veces;
+    private EditText input_puerto;
     private FloatingActionButton fab;
 
-    private ControladoraSocket m_controladora;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +40,9 @@ public class Principal extends AppCompatActivity {
         setContentView(R.layout.activity_principal);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        edit_resultados = (EditText) findViewById(R.id.edit_resultado);
+        text_resultados = (TextView) findViewById(R.id.text_resultados);
         input_direccion = (EditText) findViewById(R.id.input_direccion);
-        input_mensaje = (EditText) findViewById(R.id.input_mensaje);
+        input_puerto = (EditText) findViewById(R.id.input_puerto);
         input_cant_veces = (EditText) findViewById(R.id.input_cant_veces);
 
         fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -45,22 +51,18 @@ public class Principal extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(validaciones(v)){
-                    // TODO: 11/11/15
-                    //m_controladora = new ControladoraSocket(input_direccion.getText().toString());
+                    //Obtiene los datos
+                    int cant_veces = Integer.parseInt(input_cant_veces.getText().toString());
+                    String direccion = input_direccion.getText().toString();
+                    int puerto = Integer.parseInt(input_puerto.getText().toString());
+
+                    //Hace el numero de envios con la cantidad que digito el usuario.
+                    for(int i=0; i< cant_veces; ++i){
+                        MyClientTask myClientTask = new MyClientTask(direccion, puerto);
+                    }
                 }
             }
         });
-
-        /* //Para generar la alerta abajo.
-        * new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        }
-        * */
-
 
     }
 
@@ -84,9 +86,9 @@ public class Principal extends AppCompatActivity {
         }
         if(id==R.id.action_clear){
             input_direccion.setText("");
-            edit_resultados.setText("");
-            input_mensaje.setText("");
+            text_resultados.setText("");
             input_cant_veces.setText("");
+            input_puerto.setText("");
             return true;
         }
 
@@ -102,7 +104,7 @@ public class Principal extends AppCompatActivity {
      */
     private boolean validaciones(View v){
         if(input_direccion.getText().length() != 0) {
-            if(input_mensaje.getText().length() != 0) {
+            if(input_puerto.getText().length() != 0){
                 if(input_cant_veces.getText().length() != 0){
                     return true;
                 }else{
@@ -117,16 +119,17 @@ public class Principal extends AppCompatActivity {
                     return false;
                 }
             }else{
-                Snackbar alerta = Snackbar.make(v, "Tiene que ingresar un mensaje", Snackbar.LENGTH_LONG);
+                Snackbar alerta = Snackbar.make(v, "Tiene que ingresar un puerto", Snackbar.LENGTH_LONG);
                 alerta.setAction("Revisar", new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        input_mensaje.requestFocus();
+                        input_puerto.requestFocus();
                     }
                 });
                 alerta.show();
                 return false;
             }
+
         }else{
             Snackbar alerta = Snackbar.make(v, "Tiene que ingresar una dirección IP", Snackbar.LENGTH_LONG);
             alerta.setAction("Revisar", new View.OnClickListener() {
@@ -138,5 +141,72 @@ public class Principal extends AppCompatActivity {
             alerta.show();
             return false;
         }
+    }
+
+    /**
+     * Clase que se encarga de realizar la conexión por medio de los sockets. Lo realiza en un hilo separado ya que todas las
+     * operaciones de red se tienen que realizar en un hilo en el "background".
+     * @author Andr.oid Eric http://android-er.blogspot.com/2014/02/android-sercerclient-example-client.html
+     */
+    public class MyClientTask extends AsyncTask<Void, Void, Void> {
+
+        String dstAddress;
+        int dstPort;
+        String response = "";
+
+        MyClientTask(String addr, int port){
+            dstAddress = addr;
+            dstPort = port;
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+
+            Socket socket = null;
+
+            try {
+                socket = new Socket(dstAddress, dstPort);
+
+                ByteArrayOutputStream byteArrayOutputStream =
+                        new ByteArrayOutputStream(1024);
+                byte[] buffer = new byte[1024];
+
+                int bytesRead;
+                InputStream inputStream = socket.getInputStream();
+
+    /*
+     * notice:
+     * inputStream.read() will block if no data return
+     */
+                while ((bytesRead = inputStream.read(buffer)) != -1){
+                    byteArrayOutputStream.write(buffer, 0, bytesRead);
+                    response += byteArrayOutputStream.toString("UTF-8");
+                }
+
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+                response = "UnknownHostException: " + e.toString();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                response = "IOException: " + e.toString();
+            }finally{
+                if(socket != null){
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            text_resultados.append(response+"\n");
+            super.onPostExecute(result);
+        }
+
     }
 }
